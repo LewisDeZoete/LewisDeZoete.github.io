52602d66126a0383d85357a4fb2f82d534edcafa
LewisDeZoete <lewis.dezoete@gmail.com>
2026-02-28 01:33:12 +1100

    Prettifying

--- assets/js/article_popup.js
+++
@@ -1,9 +0,0 @@
-function openArticle() {
-document.getElementById("article-overlay").classList.add("active");
-document.body.style.overflow = "hidden";
-}
-
-function closeArticle() {
-document.getElementById("article-overlay").classList.remove("active");
-document.body.style.overflow = "";
-}
