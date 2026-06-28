a4b3d0d6a028dd29a45e7ab3b281e215bb31972b
LewisDeZoete <lewis.dezoete@gmail.com>
2026-06-28 12:33:08 +1000

    working workout_home - must fix workout page

--- projects/workouts/assets/js/workouts_home.js
+++ projects/workouts/assets/js/workouts_home.js
@@ -2,30 +2,26 @@ import { get_workout_data } from '/projects/workouts/assets/js/utils.js';
 
 // Populate workout homepage card-container
 async function get_all_workouts() {
-    const res = await fetch("/projects/workouts/assets/data/workouts_list.json");
+    const res = await fetch("/projects/workouts/assets/data/workouts.json");
     const data = await res.json();
 
     document.querySelectorAll(".workout-table").forEach(async table => {
         const workout_type = table.dataset.group;
         const tbody = table.querySelector("tbody")
 
-        // Loop over the fiels by recipe type
-        for (const workout_filename of data[workout_type]) {
-            const { title, description, workout_data } = await get_workout_data(
-                workout_type,
-                workout_filename
-            );
+        // Loop over the fields by workout type
+        for (const { workout_info, _ } of data[workout_type]) {
 
             const link = document.createElement("a");
-            link.href = `/projects/workouts/partials/workout_page.html?type=${workout_type}&file=${workout_filename}`;
-            link.textContent = title
+            link.href = `/projects/workouts/partials/workout_page.html?type=${workout_type}&file=${workout_info.title}`;
+            link.textContent = workout_info.title
 
             const titleCell = document.createElement("td");
             titleCell.appendChild(link);
 
             // --- Description preview ---
             const descriptionCell = document.createElement("td");
-            descriptionCell.textContent = description;
+            descriptionCell.textContent = workout_info.description;
 
             // --- Table row ---
             const row = document.createElement("tr");
